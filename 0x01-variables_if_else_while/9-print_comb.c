ae508cd135b5860f72a2de7ce1048e9771d29aac
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-14 13:14:16 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		putchar(number);
+		if (number == '9')
+		{
+			number++;
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
