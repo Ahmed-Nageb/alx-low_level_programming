4235b6eb7f42940edf53d0be1fca97f5bda77186
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-14 15:16:01 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,5 +10,35 @@
 
 int main(void)
 {
+	int f_iteration;
+	int s_iteration;
+	int t_iteration;
+	int fo_iteration;
+	for (f_iteration = '0'; f_iteration <= '9'; f_iteration++)
+	{
+		for (s_iteration = '0'; s_iteration <= '9'; s_iteration++)
+		{
+			for (t_iteration = f_iteration; t_iteration <= '9'; t_iteration++)
+			{
+				for (fo_iteration = s_iteration + 1; fo_iteration <= '9'; fo_iteration++)
+				{
+					putchar(f_iteration);
+					putchar(s_iteration);
+					putchar(' ');
+					putchar(t_iteration);
+					putchar(fo_iteration);
 
+					if (!((f_iteration == '9' && s_iteration == '8') &&
+						  (t_iteration == '9' && fo_iteration == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				fo_iteration = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
