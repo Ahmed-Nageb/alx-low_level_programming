6731529086ddbe2074b225444fc1f40bb5997871
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-14 12:50:04 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
