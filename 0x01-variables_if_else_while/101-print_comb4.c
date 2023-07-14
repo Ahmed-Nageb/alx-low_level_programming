f915735bcea4711502665ea836cdb4b7f2f253fa
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-14 14:42:16 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,8 @@ int main(void)
 	int f_iteration;
 	int s_iteration;
 	int t_iteration;
-	
+
+	/*Code*/	
 	for (f_iteration = '0'; f_iteration <= '9' ; f_iteration++)
 	{
 		for (s_iteration = f_iteration; s_iteration <= '9' ; s_iteration++)
