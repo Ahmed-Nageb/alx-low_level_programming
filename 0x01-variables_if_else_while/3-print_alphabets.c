bb86b56dbacd6e13a3ba3d7eafb7e3217ac555ae
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-14 12:28:17 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int chr;
+
+	chr = 'a';
+	while (chr <= 'z')
+	{
+		putchar(chr);
+		chr++;
+	}
+	chr = 'A';
+        while (chr <= 'Z')
+	{
+		putchar(chr);
+		chr++;
+	}
+	putchar('\n');
+	return (0);
+}
