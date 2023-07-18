7ddec54ed1072f19e89da43e43a10ac425ff16f1
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-18 15:30:29 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * print_last_digit - return the abs value
+ * @n: The number to be checked
+ *
+ * Return: %10 of n
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = (n%10);
+	_putchar(last + '0');
+	return (last);
+}
