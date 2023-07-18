0b9e97f451a829af2b2220e5f576a873e39e6d2a
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-18 14:14:10 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * _isalpha - checks for _isalpha character
+ * @c: The character to be checked
+ * ASCII 65->90 :Capital letters
+ *	97->122 :Lowercase letters
+ * Return: 1 if the number is letter , 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >=65 && c <= 90))
+		return (1);
+	else
+		return (0);
+
+
+}
