8e2308c2490521fe6ff9a114e2a2bf068a612649
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-18 11:59:37 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * print_alphabet_x10 - Function implementation
+ *
+ * ASCII 65->90 :Capital letters
+ * 	 97->122:Lowercase letters
+ * Return: 1 if c is lowercase , 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122 )
+		return 1;
+	else
+		return 0;
+
+
+}
