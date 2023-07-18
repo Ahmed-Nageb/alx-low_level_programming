44a9087dc7633fc90301c4024b6a8549ccbb18fb
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-18 10:58:20 -0700

    1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+void print_alphabet_x10(void)
+{
+	unsigned char i;
+	unsigned char counter ;
+
+	counter = 0;
+	while (counter <= 10)
+	{
+
+		for(i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		counter ++;
+	}
+}
