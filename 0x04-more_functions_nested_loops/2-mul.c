5a711d93b9c8baa5a3a9e3120e1eb7665b196738
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-20 11:21:21 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - checks for lowercase character
+ * @a: The first input
+ * @b: The second input
+ * Return: the mul of the two inputs
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
