327312b725d8c6311d4df95c337328b07d7fc761
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-20 11:12:44 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,12 @@
 /**
  * _isupper - checks for lowercase character
  * @c: The character to be checked
- * ASCII 65->90 :Capital letters
- *	97->122 :Lowercase letters
  * Return: 1 if the number is lowercase , 0 otherwise
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 0 && c <= 9)
 		return (1);
 	else
 		return (0);
