3573d80cc769833cd877cfbb9a596eefad2f06a2
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-20 11:14:48 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - checks for lowercase character
+ * _isdigit - checks for lowercase character
  * @c: The character to be checked
- * Return: 1 if the number is lowercase , 0 otherwise
+ * Return: 1 if the number is digit , 0 otherwise
  */
 
 int _isdigit(int c)
