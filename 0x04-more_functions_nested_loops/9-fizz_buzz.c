29172d54bea9fd48e16a606639fe1c628ce190dc
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-20 14:39:47 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{	
+	int counter;
+
+	for (counter = 1; counter <= 100; counter++)
+	{
+		if (counter != 1)
+			printf(" ");
+		if (counter % 15 == 0)
+			printf("FizzBuzz");
+		else if (counter % 3 == 0)
+			printf("Fizz");
+		else if (counter % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d ", counter);
+	}
+	printf("\n");
+	return (0);
+}
