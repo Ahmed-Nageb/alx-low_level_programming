4cfc9b19d2c9be33c3c2597e0ecb7e58d43a453c
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-26 23:11:34 +0300

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ **string_toupper - function that concat. a string to another one.
+ *@s: input. string
+ *
+ *Return: string
+ */
+char *string_toupper(char *s)
+{
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{ *s = *s - ' ';
+		}
+
+		s++;
+	}
+
+	return (s);
+}
