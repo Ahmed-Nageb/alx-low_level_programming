0707c7f5d4a82c89707ce3bb896ff6523a302889
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-27 00:01:49 +0300

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *rot13 - check the code for Holberton School students.
+ *@s: String to be converted
+ *Return: Always 0.
+ */
+char *rot13(char *s)
+{
+	char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char str2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	int iter1, iter2;
+
+	for (iter2 = 0; s[iter2] != '\0'; iter2++)
+	{
+		for (iter1 = 0; str1[iter1] != '\0'; iter1++)
+		{
+			if (s[iter2] == str1[iter1])
+				s[iter2] = str2[iter1];
+		}
+	}
+
+	return (s);
+}
