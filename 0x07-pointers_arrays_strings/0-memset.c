222e424da950c36cb2a8d3f60ff2688d3d6efc9b
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-31 18:47:23 +0300

    Creating_Files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+/**
+ * *_memset - prints buffer in hexa
+ * @*s: the address of memory to print
+ * @b: the size of the memory to print
+ * @n: 
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
