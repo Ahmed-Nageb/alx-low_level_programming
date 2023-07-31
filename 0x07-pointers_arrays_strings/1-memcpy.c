1eca90a2ca6cac38d0b241000734ec60e7c89c36
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-31 19:08:48 +0300

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,18 @@
-char *_memcpy(char *dest, char *src, unsigned int n);
+/**
+ **_memcpy - a function that fills memory with a constant byte.
+ *@dest: the address of memory that will copy to
+ *@src: the address of memory that will copy from
+ *@n: number of bytes
+ *Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n)
+	{
+		dest[n - 1] = src[n - 1];
+		n--;
+	}
+
+	return (dest);
+}
