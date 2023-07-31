0a645ab18680a1cc74c46e5853dd99a3fb4d59f6
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-31 19:22:58 +0300

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,29 @@
-char *_strchr(char *s, char c);
+#include "main.h"
+
+/**
+ **_strchr - a function that copies memory area.
+ *@dest: the address of memory that will copy to
+ *@src: the address of memory that will copy from
+ *@n: number of bytes
+ *Return: Nothing.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int iter = 0;
+
+	while (s[iter] != '\0')
+	{
+		if (s[iter] == c)
+		{
+			s = &s[iter];
+			return (s);
+		}
+		else
+		{
+			iter++;
+		}
+	}
+
+	return (0);
+}
