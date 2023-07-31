100f3e1dcf5c2307f0b15d899e39c72d0507891b
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-07-31 18:55:54 +0300

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,18 @@
 /**
- * *_memset - prints buffer in hexa
- * @*s: the address of memory to print
- * @b: the size of the memory to print
- * @n: 
- * Return: Nothing.
+ **_memset - a function that fills memory with a constant byte.
+ *@*s: the address of memory
+ *@b: constant byte 
+ *@n: number of bytes 
+ *Return: Nothing.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
 
+	return (s);
 }
