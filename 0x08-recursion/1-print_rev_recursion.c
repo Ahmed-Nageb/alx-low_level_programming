13373b11a20e70f9854e206055c1fbf3889ad465
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-08-03 05:07:53 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - a function that fills memory with a constant byte.
+ *@s: the address of memory
+ *Return: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
+}
