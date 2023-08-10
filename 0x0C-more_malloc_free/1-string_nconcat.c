2c43f25e8af94cccd6df1bb3f2ba1c9d11946ecc
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-08-10 22:24:28 +0300

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
  *string_nconcat - concatenates two strings
  *@s1: destination string
@@ -18,38 +16,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len2 = 0;
 	counter = 0;
 	counter2 = 0;
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[len])
 		len++;
 	while (s2[len2])
 		len2++;
-
 	if (n >= len2)
 		n = len2;
-
 	s = malloc(sizeof(char) * len + n + 1);
 	if (!s)
 		return (0);
-
 	while (s1[counter] != '\0')
 	{
 		s[counter] = s1[counter];
 		counter++;
 	}
-
 	while (s2[counter2] != '\0' && counter2 <= n)
 	{
 		s[counter] = s2[counter2];
 		counter++;
 		counter2++;
 	}
-
 	s[len + n] = '\0';
-
 	return (s);
 }
