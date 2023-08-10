8ccce1ece4d6b480f4a3fae5db7b0869253fccf1
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-08-10 22:14:26 +0300

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,43 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *_strdup - concatenates two strings
- *@str: number of characters to be copied
- *Return: a pointer to a new str which is a duplicate str,NULL if str = NULL
+ *string_nconcat - concatenates two strings
+ *@s1: destination string
+ *@s2: source string
+ *@n: number of characters to be copied
+ *Return: copied
  */
-
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len, counter;
+	unsigned int len, counter, counter2;
 	char *s;
 
 	len = 0;
 	counter = 0;
-	while (str[len])
+	counter2 = 0;
+	while (s1[len])
 		len++;
 
-	if (!len)
-		return (0);
-
-	s = malloc(len + 1);
+	s = malloc(len + n + 1);
 	if (!s)
 		return (0);
 
-	while (counter < len)
+	while (s1[counter] != '\0')
 	{
-		s[counter] = str[counter];
+		s[counter] = s1[counter];
 		counter++;
 	}
+
+	while (s2[counter2] != '\0' && counter2 <= n)
+	{
+		s[counter] = s2[counter2];
+		counter++;
+		counter2++;
+	}
+
+	s[len + n + 1] = '\0';
 
 	return (s);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,4 @@
 int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
-char *_strdup(char *str);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
